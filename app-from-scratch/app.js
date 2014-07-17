b7da479a872ba6495e8c8895ecbc61431a35f91a
shikevin <im@shikev.in>
2014-07-17 18:05:48 -0400

    adding more jade

--- app-from-scratch/app.js
+++ app-from-scratch/app.js
@@ -14,6 +14,20 @@ app.all('*', function(req,res) {
 });
 
 //pass in express app object with configurations to createServer
-http.createServer(app).listen(app.get('port'), function() {
-    console.log('Express server listening on port ' + app.get('port'));
-})
+var server = http.createServer(app);
+var boot = function() {
+    server.listen(app.get('port'), function () {
+        console.log('Express server listening on port ' + app.get('port'));
+    })
+}
+var shutdown = function() {
+    server.close();
+}
+if(require.main === module){
+    boot();
+} else {
+    console.info('Running app as a module')
+    exports.boot = boot;
+    exports.shutdown = shutdown;
+    exports.port = app.get('port');
+}
