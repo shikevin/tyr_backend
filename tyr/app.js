33352b8d03e5f269107da908fb367c7150e44bef
shikevin <im@shikev.in>
2014-07-25 22:45:23 -0400

    adding schema to mongoose

--- tyr/app.js
+++ tyr/app.js
@@ -1,3 +1,21 @@
 var mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/test');
+
+var User = mongoose.model('User', { name:String });
+
+var newUser = new User({ name: 'Kevin Shi' });
+
+var userSChema = mongoose.Schema({
+    name: String
+});
+
+newUser.save(function (err, results) {
+    if (err) {
+        console.error(e);
+        process.exit(1);
+    } else {
+        console.log('Saved: ', results);
+        process.exit(0);
+    }
+});
