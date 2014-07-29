6da5ed3e01c96137e42159e71d57e673b781c3f7
shikevin <im@shikev.in>
2014-07-29 19:28:10 -0400

    first part of chat app done

--- tyr_final/app.js
+++ tyr_final/app.js
@@ -3,16 +3,18 @@ var path = require('path');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 var http = require('http');
-var io = require('socket.io').listen(server);
-
-//var routes = require('./routes/index');
-//
-//app.use('/', routes);
 
 var app = express();
 var server = http.createServer(app);
+var io = require('socket.io').listen(server);
 server.listen(3000);
 
 app.get('/', function(req,res){
     res.sendfile(__dirname + '/index.html')
+});
+
+io.sockets.on('connection', function(socket){
+   socket.on('send message', function(data){
+       io.sockets.emit('new message', data);
+   });
 });
